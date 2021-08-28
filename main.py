1ac85fa757ccf5b9e6b9f601228e4ebbba323d0f
Neel Adwani <nprosofficial@gmail.com>
2021-08-28 18:33:15 +0000

    removed redundant images

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import discord
 import os
 from keep_alive import keep_alive
+from PIL import Image
 
 client = discord.Client()
 image_types = ["png", "jpeg", "gif", "jpg"]
@@ -15,9 +16,13 @@ async def on_message(message):
   if message.author != client.user:
     for attachment in message.attachments:
       if any(attachment.filename.lower().endswith(image) for image in image_types):
-        await attachment.save('output.png')
+        await attachment.save('input.png')
         # await message.reply(file=attachment.filename)
         await message.reply("Here's your framed pic!\n")
+        img1 = Image.open(r"frame.png")
+        img2 = Image.open(r"input.png")
+        img1.paste(img2, (0,0))
+        img1.save('output.png')
         with open('output.png', 'rb') as f:
           await message.channel.send(file=discord.File(f))
 
