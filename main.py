662b5ccf4516803323211abe38c6d0b3cfb952e2
Neel Adwani <nprosofficial@gmail.com>
2021-08-28 14:38:11 +0000

    upload attachment fix

--- main.py
+++ main.py
@@ -14,7 +14,11 @@ async def on_message(message):
   if message.author != client.user:
     for attachment in message.attachments:
       if any(attachment.filename.lower().endswith(image) for image in image_types):
-        await attachment.save(attachment.filename)
+        # await attachment.save(attachment.filename)
+        # await message.reply(file=attachment.filename)
+        await message.reply("Here's your framed pic!\n")
+        with open('sv.png', 'rb') as f:
+          await message.channel.send(file=discord.File(f))
 
 token = os.environ.get("DISCORD_BOT_SECRET")
 client.run(token)
