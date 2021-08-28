4ae0eee6f917cf498e73dd43b08ece6674d7acad
Neel Adwani <nprosofficial@gmail.com>
2021-08-28 18:07:51 +0000

    fixed keep_alive

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import discord
 import os
+from keep_alive import keep_alive
 
 client = discord.Client()
 image_types = ["png", "jpeg", "gif", "jpg"]
@@ -14,11 +15,12 @@ async def on_message(message):
   if message.author != client.user:
     for attachment in message.attachments:
       if any(attachment.filename.lower().endswith(image) for image in image_types):
-        # await attachment.save(attachment.filename)
+        await attachment.save('output.png')
         # await message.reply(file=attachment.filename)
         await message.reply("Here's your framed pic!\n")
-        with open('sv.png', 'rb') as f:
+        with open('output.png', 'rb') as f:
           await message.channel.send(file=discord.File(f))
 
+keep_alive()
 token = os.environ.get("DISCORD_BOT_SECRET")
 client.run(token)
